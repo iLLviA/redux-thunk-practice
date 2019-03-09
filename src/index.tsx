3b7c24b22703da4a481008d59c13b8b3e416d871
iLLviA <illvia.3594@gmail.com>
2019-03-10 03:02:42 +0900

    ok

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import Hello from './componetns/Search'
 
 class App extends React.Component {
   render() {
     return (
         <div>
-          <h1>Hello React!</h1>
+          <Hello greeting="Hello!" />
         </div>
     );
   }
