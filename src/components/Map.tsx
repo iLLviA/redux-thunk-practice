7254453d7afcc7331417c09717087e9b4fc25bd9
iLLviA <illvia.3594@gmail.com>
2019-05-07 02:05:09 +0900

    googlemap追加

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,9 +1,47 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
+import { RootState } from '../modules'
 
-const Map = () => {
-    
+type Props = {
+    location: {
+        lat: number;
+        lng: number;
+    }
+}
+
+const InnerMap = withScriptjs(withGoogleMap((props:Props) => {
+    return (
+    <GoogleMap
+        defaultZoom = {12}
+        defaultCenter= {props.location}
+        center = {props.location}
+    >
+    </GoogleMap>
+    )
+}
+))
+
+const Map = (props:Props) => {
+
+    return (
+    <InnerMap 
+        location ={ props.location } 
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyArdDInOBYawxpt97Jp7z1EjrtaLpgzv3Q&v=3.exp&libraries=geometry,drawing,places"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `400px`, width: `350px` }} />}
+        mapElement={<div style={{ height: `100%` }} />} 
+    />
+    )
+}
+
+const mapStateToProps = (state:RootState):Props => {
+    return {
+        location: state.place.location
+    }
 }
 
 
-export default Map
+export default connect(
+    mapStateToProps
+)(Map)
--- src/pages/HotelSearch.tsx
+++ src/pages/HotelSearch.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import Form from '../components/Form'
 import HotelArea from '../components/HotelsArea'
 import styled from 'styled-components'
+import Map from '../components/Map'
 
 const HotelSearch = () => {
 
@@ -10,6 +11,7 @@ const HotelSearch = () => {
             <h1>ホテル検索</h1>
             <Form />
             <HotelArea />
+            {/* <Map /> */}
         </Wrapper>
     )
 }
